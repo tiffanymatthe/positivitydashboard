fe7d2f21a8a12b971a77830457fb68c61db3acf4
Tiffany Matthe <tiffanymatthe@gmail.com>
2021-03-07 07:06:39 -0800

    Add better cleardb config for Heroku.

--- config.php
+++ config.php
@@ -20,7 +20,14 @@ if ($local) {
 	define('ROOT_PATH', realpath(dirname(__FILE__)));
 	define('BASE_URL', 'http://localhost/');
 } else {
-	$conn = mysqli_connect("us-cdbr-east-03.cleardb.com", "bd43e8f13d58e6", "61f99b4e", "positivity-dashboard");
+	$url = parse_url(getenv("mysql://bd43e8f13d58e6:61f99b4e@us-cdbr-east-03.cleardb.com/heroku_ce1583fd0a7a864?reconnect=true"));
+
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
+
+	$conn = new mysqli($server, $username, $password, $db);
 
 	if (!$conn) {
 		die("Error connecting to database: " . mysqli_connect_error());
